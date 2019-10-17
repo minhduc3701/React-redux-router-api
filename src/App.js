0efb83fa56ee0547e30fd957e6f39af187dac722
minhduc <minh.duc.3701@gmail.com>
2019-10-17 16:05:17 +0700

    Design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="navbar navbar-default">
+          <a className="navbar-brand">CALL-API</a>
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a>Trang Chủ</a>
+            </li>
+            <li>
+              <a>Quản Lý Sản Phẩm</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <button type="button" className="btn btn-info mb-10">
+                Thêm Sản Phẩm
+              </button>
+
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  <h3 className="panel-title">Danh Sách Sản Phẩm</h3>
+                </div>
+                <div className="panel-body">
+                  <table className="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>STT</th>
+                        <th>Mã</th>
+                        <th>Tên</th>
+                        <th>Giá</th>
+                        <th>Trạng Thái</th>
+                        <th>Hoạt Động</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>Iphone 6 plus</td>
+                        <td>200$</td>
+                        <td>
+                          <span className="label label-warning">Còn Hàng</span>
+                        </td>
+                        <td>
+                          <button
+                            type="button"
+                            className="btn btn-success mr-10"
+                          >
+                            Sửa
+                          </button>
+
+                          <button type="button" className="btn btn-danger">
+                            Xóa
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
