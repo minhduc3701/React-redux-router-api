eb9ed93cd66c1b3be7dc3547a391b89f8874cc10
minhduc <minh.duc.3701@gmail.com>
2019-10-18 10:57:03 +0700

    Add Config (axios)

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import ProductList from "./../../components/ProductList/ProductList";
 import ProductItem from "./../../components/ProductItem/ProductItem";
-import { connect } from "react-redux";
-import axios from "axios";
+import callApi from "./../../utils/apiCaller";
 
 class ProductListPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    callApi("products", null).then(res => {
+      this.setState({
+        products: res.data
+      });
+    });
+  }
+
   showProducts = products => {
     let result = null;
     if (products.length > 0) {
@@ -23,20 +39,7 @@ class ProductListPage extends React.Component {
 
   render() {
     // let { products } = this.props;
-    let products = [];
-    axios({
-      method: "GET",
-      url: "http://localhost:3000/products",
-      data: null
-    })
-      .then(res => {
-        products = res.data;
-      })
-      .catch(err => {
-        console.log(err);
-      });
-
-    console.log(products);
+    let { products } = this.state;
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <button type="button" className="btn btn-info mb-10">
