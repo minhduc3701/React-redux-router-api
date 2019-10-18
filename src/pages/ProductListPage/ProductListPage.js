e60f23ac92cd761eb51ea1f08d9992ee336742b7
minhduc <minh.duc.3701@gmail.com>
2019-10-18 10:33:54 +0700

    Add axios

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProductList from "./../../components/ProductList/ProductList";
 import ProductItem from "./../../components/ProductItem/ProductItem";
 import { connect } from "react-redux";
+import axios from "axios";
 
 class ProductListPage extends React.Component {
   showProducts = products => {
@@ -21,7 +22,20 @@ class ProductListPage extends React.Component {
   };
 
   render() {
-    let { products } = this.props;
+    // let { products } = this.props;
+    let products = [];
+    axios({
+      method: "GET",
+      url: "http://localhost:3000/products",
+      data: null
+    })
+      .then(res => {
+        products = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
     console.log(products);
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
